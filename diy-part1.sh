bc707472124d464d0e0cc1654b6c5b79688ab6c6
one02free <one02free@qq.com>
2023-11-20 11:31:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,11 +12,11 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;main' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone -b main https://github.com/fw876/helloworld.git package/lean/helloworld
+#git clone -b main https://github.com/fw876/helloworld.git package/lean/helloworld
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config

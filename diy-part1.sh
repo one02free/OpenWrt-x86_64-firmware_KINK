3ae135f96e016db4a37079acfb7b7c8c8eaf451a
one02free <3870416+one02free@users.noreply.github.com>
2024-10-26 18:44:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,6 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.con
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone -b main https://github.com/fw876/helloworld.git package/lean/helloworld
-rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon && git git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 # rm -rf package/feeds/packages/uugamebooster && git clone https://github.com/QC3284/uugamebooster.git package/feeds/packages/uugamebooster
